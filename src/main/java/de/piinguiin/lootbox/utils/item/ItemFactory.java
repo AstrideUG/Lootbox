9d022907baa457d84404799f0251e26219090592
Piinguiin <neuraxhd@gmail.com>
2019-06-26 17:00:23 +0200

    ItemFactory added lootbox head function

--- src/main/java/de/piinguiin/lootbox/utils/item/ItemFactory.java
+++ src/main/java/de/piinguiin/lootbox/utils/item/ItemFactory.java
@@ -1,4 +1,35 @@
 package de.piinguiin.lootbox.utils.item;
 
+import org.bukkit.inventory.ItemStack;
+
 public class ItemFactory {
+
+    public static ItemStack getAnimationHead(final String id) {
+
+        final SkullMaker skullMaker = new SkullMaker();
+
+        switch (id) {
+
+            case "moon":
+                skullMaker.withName("§9§lMoonBox");
+                skullMaker.withSkinUrl("http://textures.minecraft.net/texture/8fba5dbee2a732d053618a3998f986c8f56db40fe314a0ce028501f5e54b73d3");
+                break;
+
+            case "galactic":
+                skullMaker.withName("§e§lGalacticBox");
+                skullMaker.withSkinUrl("http://textures.minecraft.net/texture/87d66f93909a6d4641c653082e04749691de82cf77232bd20ab32adf4f");
+                break;
+
+
+            //§a§lC§b§lo§e§ls§6§lm§c§3li§4§lc
+            case "cosmic":
+                skullMaker.withName("§c§lC§6§lo§e§ls§a§lm§b§li§3§lc§9§lB§5§lo§d§lx");
+                skullMaker.withSkinUrl("http://textures.minecraft.net/texture/9d3d250e25bbca3a62be5b3ef02cfcab6dcdc424884c9a7d5cc95c9d0");
+                break;
+
+        }
+
+        return skullMaker.build();
+    }
+
 }
